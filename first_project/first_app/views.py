449f2c4cfeb6a653ab490675a25960d140fd0516
HaiNghi <lehainghi29@gmail.com>
2017-12-08 17:17:05 +0700

    update db

--- first_project/first_app/views.py
+++ first_project/first_app/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from first_app.models import Topic,AccessRecord,Webpage
 
+# def index(request):
+#     # return HttpResponse("Hello world!")
+#     my_dict={'insert_me':'Iam nickie!'}
+#     return render(request,'index.html',context=my_dict)
+# # Create your views here.
 def index(request):
-    # return HttpResponse("Hello world!")
-    my_dict={'insert_me':'Iam nickie!'}
-    return render(request,'index.html',context=my_dict)
-# Create your views here.
+    webpage_list=AccessRecord.objects.order_by('date')
+    date_dict={'access_records':webpage_list}
+    return render(request, 'first_app/index.html', context=date_dict)
 def index2(request):
     # return HttpResponse("Hello world!")
     my_dict={'insert_me':'Iam nickie!'}
--- first_project/popular_first_app.py
+++ first_project/popular_first_app.py
@@ -7,14 +7,14 @@ django.setup()
 ##FAKE POP SCRIPT
 
 import random
-from first_app.models import AccessRecord,Topic,Webpage
+from first_app.models import AccessRecord, Topic, Webpage
 from faker import Faker
 
-faker=Faker()
+faken=Faker()
 topics=['Search','Social','Marketplace','News','Games']
 
 def add_topic():
-    t=Topic.objects.get_or_create(top_name=random.choice(topics)[0])
+    t=Topic.objects.get_or_create(top_name=random.choice(topics))[0]
     t.save()
     return t
 
@@ -24,4 +24,18 @@ def popular(N=5):
         # get topic for the entry
         top=add_topic()
         #create the fake data for that entry
+        fake_url= faken.url()
+        fake_date= faken.date()
+        fake_name=faken.company()
+
+        #create the new webpage entry
+        webg=Webpage.objects.get_or_create(topic=top,url=fake_url,name=fake_name)[0]
+
+        #create a fake access record for that webpage
+        acc_rec =AccessRecord.objects.get_or_create(name=webg, date=fake_date)[0]
+
+if __name__ == '__main__':
+    print('populating script!')
+    popular(20)
+    print('populating complete!')
 
